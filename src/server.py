413630de9a952fd7525a19d454f1408ba7ab12a1
Roman Kolerov <coder8080@yandex.ru>
2025-10-18 18:51:12 +0300

    More modes

--- src/server.py
+++ src/server.py
@@ -14,14 +14,40 @@ def set_solid(color: tuple[int, int, int]):
         color: желаемый цвет
     """
     visual.send_command(AnimationType.SOLID, [color])
+    return "OK"
 
 
 @mcp.tool()
 def set_rainbow():
     """
-    Установить радужное переливание
+    Установить радужное переливание по кругу
     """
-    visual.send_command(AnimationType.RAINBOW)
+    visual.send_command(AnimationType.RAINBOW, [])
+    return "OK"
+
+
+@mcp.tool()
+def set_breath(color: tuple[int, int, int]):
+    """
+    Установить эффект дыхания - плавное пульсирование цвета
+
+    Args:
+        color: желаемый цвет
+    """
+    visual.send_command(AnimationType.BREATH, [color])
+    return "OK"
+
+
+@mcp.tool()
+def set_loop(colors: list[tuple[int, int, int]]):
+    """
+    Установить последовательное переливание несколькими цветами
+
+    Args:
+        colors: цвета, которыми лента должна переливаться
+    """
+    visual.send_command(AnimationType.LOOP, colors)
+    return "OK"
 
 
 def run_mcp():
--- src/visual.py
+++ src/visual.py
@@ -1,10 +1,11 @@
 import sys
 from colorsys import hsv_to_rgb
 from enum import Enum
-from math import cos, pi, sin
+from math import cos, floor, pi, sin
 from queue import Queue
 
 import pygame
+from pydantic import TypeAdapter
 
 WIDTH = 600
 HEIGHT = 600
@@ -16,23 +17,28 @@ FLAGS = pygame.SCALED | pygame.RESIZABLE  # без OPENGL
 
 
 class Pixel:
-    x: int
-    y: int
+    x: float
+    y: float
     color: tuple[int, int, int]
 
     def __init__(self, number: int):
         angle = 2 * pi / TOTAL_PIXELS * number
         self.x = CIRCLE_RADIUS * cos(angle) + WIDTH / 2
-        self.y = -1 * CIRCLE_RADIUS * sin(angle) + HEIGHT / 2
+        self.y = -CIRCLE_RADIUS * sin(angle) + HEIGHT / 2
         self.color = (255, 255, 255)
 
     def draw(self, surface: pygame.Surface):
         pygame.draw.circle(surface, self.color, (self.x, self.y), PIXEL_RADIUS)
 
 
+ColorAdapter = TypeAdapter(tuple[int, int, int])
+
+
 class AnimationType(Enum):
     SOLID = "solid"
     RAINBOW = "rainbow"
+    BREATH = "breath"
+    LOOP = "loop"
 
 
 class Command:
@@ -63,7 +69,36 @@ class Visual:
         move = (self.animation_frames % K) / K
         for i, pixel in enumerate(self.pixels):
             hue = i / TOTAL_PIXELS + move
-            pixel.color = tuple(round(j * 255) for j in hsv_to_rgb(hue % 1, 1, 1))
+            pixel.color = ColorAdapter.validate_python(
+                tuple(round(j * 255) for j in hsv_to_rgb(hue % 1, 1, 1))
+            )
+
+    def animate_breath(self):
+        N = 15
+        V = (sin(self.animation_frames / N) + 1) / 2
+        color = ColorAdapter.validate_python(
+            tuple([floor(val * V) for val in self.animation_colors[0]])
+        )
+        for pixel in self.pixels:
+            pixel.color = color
+
+    def animate_loop(self):
+        k = 60
+        n = k * len(self.animation_colors)
+        m = self.animation_frames % n
+        i = m // k
+        j = (i + 1) % len(self.animation_colors)
+        v2 = m % k
+        v1 = k - v2
+        k1 = v1 / k
+        k2 = v2 / k
+        c1 = self.animation_colors[i]
+        c2 = self.animation_colors[j]
+        c = ColorAdapter.validate_python(
+            tuple([floor(k1 * c1[x] + k2 * c2[x]) for x in range(3)])
+        )
+        for pixel in self.pixels:
+            pixel.color = c
 
     def send_command(self, type: AnimationType, colors: list[tuple[int, int, int]]):
         item = Command()
@@ -76,9 +111,13 @@ class Visual:
         self.display = pygame.display.set_mode((WIDTH, HEIGHT), FLAGS)
         self.pixels = [Pixel(i) for i in range(TOTAL_PIXELS)]
 
-        self.animation_type: AnimationType = AnimationType.RAINBOW
-        self.animation_colors: list[tuple[int, int, int]] = [[255, 0, 0]]
-        self.animation_frames: int = 0
+        self.animation_type = AnimationType.LOOP
+        self.animation_colors = [
+            (255, 0, 0),
+            (0, 255, 0),
+            (0, 0, 255),
+        ]
+        self.animation_frames = 0
         self.q = Queue()
 
         while True:
@@ -98,6 +137,10 @@ class Visual:
                 self.animate_solid()
             elif self.animation_type == AnimationType.RAINBOW:
                 self.animate_rainbow()
+            elif self.animation_type == AnimationType.BREATH:
+                self.animate_breath()
+            elif self.animation_type == AnimationType.LOOP:
+                self.animate_loop()
 
             for pixel in self.pixels:
                 pixel.draw(self.display)
